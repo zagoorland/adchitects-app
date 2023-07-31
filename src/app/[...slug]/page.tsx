94e858189fd086b6df62d35b7c075b0f6f023142
zagoorland <zagoorland@gmail.com>
2023-07-31 11:26:15 +0200

    changed margin

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <div className="flex justify-center min-h-screen">
-      <h1 className="text-2xl mt-8">
+      <h1 className="text-2xl mt-16">
         🏗️ Page {`"${params.slug}"`} is under construction
       </h1>
     </div>
